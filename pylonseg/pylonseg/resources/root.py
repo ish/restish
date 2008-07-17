2f36bf50ca8abdc43ec07922cf651a3baccc20ba
Matt Goodall <matt@pumpkin.(none)>
2008-07-17 20:36:43 +0100

    Add a child resource.

--- pylonseg/pylonseg/resources/root.py
+++ pylonseg/pylonseg/resources/root.py
@@ -4,5 +4,15 @@ from restism import http, resource
 class RootResource(resource.Resource):
 
     def __call__(self, request):
-        return http.ok([('Content-Type', 'text/plain')], 'Yay!')
+        return http.ok([('Content-Type', 'text/plain')], "I'm a RootResource!")
+
+    def resource_child(self, request, segments):
+        if segments[0] == 'foo':
+            return FooResource(), segments[1:]
+
+
+class FooResource(resource.Resource):
+
+    def __call__(self, request):
+        return http.ok([('Content-Type', 'text/html')], "<p>I'm a <strong>FooResource</strong>!</p>")
 
