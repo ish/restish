4a670130f2ae677bbdf6223459e586abca210839
Matt Goodall <matt@jdi-associates.com>
2008-07-18 16:05:33 +0100

    Add more response convenience functions to the http module and group them into the sections from the HTTP RFC.

--- restism/http.py
+++ restism/http.py
@@ -25,11 +25,35 @@ class Response(object):
         return getattr(self._response, name)
 
 
+# Successful 2xx
 
 def ok(headers, content):
     return Response("200 OK", headers, content)
 
 
+# Redirection 3xx
+
+def moved_permanently(location):
+    return Response("301 Moved Permanently", [('Location', location)], "")
+
+def found(location):
+    return Response("302 Found", [('Location', location)], "")
+
+def see_other(location):
+    return Response("303 See Other", [('Location', location)], "")
+
+def not_modified():
+    return Response("304 Not Modified", [], "")
+
+
+# Client Error 4xx
+
+def bad_request():
+    return Response("400 Bad Request", [], "")
+
 def not_found(headers, content):
     return Response("404 Not Found", headers, content)
+
+def method_not_allowed(allow):
+    return Response("405 Method Not Allowed", [('Allow', allow)], "405 Method Not Allowed")
 
