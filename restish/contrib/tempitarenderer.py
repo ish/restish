f8defd39d5fd0a6ea6a1a948597a9fad90fd5a2a
Matt Goodall <matt@jdi-associates.com>
2009-02-26 23:49:18 +0000

    Fixup the TempitaRenderer so stuff like {{inherit}} works.
    I can't decide if I'm being an idiot or if Tempita's get_file_template
    is totally wrong (it seems to join two filenames together). Also, it's
    hopelessly insecure. So, I wrote my own version of get_file_template
    that a) actually works and, b) doesn't allow you to reference files
    outside of the template directory.
    
    And to think I only really wanted Tempita support for example code (if
    you use "paster --create" then you've aleady got Tempita installed).
    :sigh:

--- restish/contrib/tempitarenderer.py
+++ restish/contrib/tempitarenderer.py
@@ -29,6 +29,23 @@ class TempitaFileSystemLoader(object):
         self.encoding = encoding
 
     def get_template(self, template):
+        template = template.lstrip('/')
         filename = os.path.join(self.directory, template)
-        return tempita.Template.from_filename(filename, encoding=self.encoding)
+        return self._get_template(filename)
+
+    def _get_template(self, filename):
+        filename = os.path.abspath(filename)
+        if os.path.commonprefix([filename, self.directory]) != self.directory:
+            raise Exception("Illegal template: outside template directory")
+        return tempita.Template.from_filename(filename, encoding=self.encoding,
+                                              get_template=self._tempita_get_template)
+
+    def _tempita_get_template(self, name, from_template):
+        if name.startswith('/'):
+            basedir = self.directory
+            name = name.lstrip('/')
+        else:
+            basedir = os.path.dirname(from_template.name)
+        filename = os.path.join(basedir, name)
+        return self._get_template(filename)
 
