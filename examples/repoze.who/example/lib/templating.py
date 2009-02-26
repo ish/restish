0d6d48be40d4958b15425ab1d2c891a78ba11246
Matt Goodall <matt@jdi-associates.com>
2009-02-26 16:33:33 +0000

    Add some example resources and templates for demonstrating authentication.

--- examples/repoze.who/example/lib/templating.py
+++ examples/repoze.who/example/lib/templating.py
@@ -22,34 +22,11 @@ def make_renderer(app_conf):
     """
     Create and return a restish.templating "renderer".
     """
-
-    # Uncomment for an example of Mako templating support.
-    #import pkg_resources
-    #import os.path
-    #from restish.contrib.makorenderer import MakoRenderer
-    #return MakoRenderer(
-    #        directories=[
-    #            pkg_resources.resource_filename('example', 'templates')
-    #            ],
-    #        module_directory=os.path.join(app_conf['cache_dir'], 'templates'),
-    #        input_encoding='utf-8', output_encoding='utf-8',
-    #        default_filters=['unicode', 'h']
-    #        )
-
-    # Uncomment for an example of Jinja2 templating support.
-    #from restish.contrib.jinja2renderer import Jinja2Renderer
-    #from jinja2 import PackageLoader
-    #return Jinja2Renderer(
-    #        loader=PackageLoader('example', 'templates'),
-    #        autoescape=True
-    #        )
-
-    # Uncomment for an example of Genshi templating support.
-    #from restish.contrib.genshirenderer import GenshiRenderer
-    #import genshi.template.loader
-    #return GenshiRenderer(
-    #        genshi.template.loader.package('example', 'templates')
-    #        )
-
-    return None
+    import pkg_resources
+    from restish.contrib.tempitarenderer import TempitaRenderer, TempitaFileSystemLoader
+    return TempitaRenderer(
+            TempitaFileSystemLoader(
+                pkg_resources.resource_filename('example', 'templates')
+                )
+            )
 
--- examples/repoze.who/example/resource/root.py
+++ examples/repoze.who/example/resource/root.py
@@ -1,13 +1,57 @@
 import logging
-from restish import http, resource
+from restish import http, resource, templating
 
 
 log = logging.getLogger(__name__)
 
 
 class Root(resource.Resource):
-    @resource.GET()
+
+    @resource.GET(accept='html')
+    @templating.page('root.html')
     def html(self, request):
-        return http.ok([('Content-Type', 'text/html')],
-            "<p>Hello from example!</p>")
+        return {}
+
+    @resource.child()
+    def open(self, request, segments):
+        return OpenResource()
+
+    @resource.child()
+    def closed(self, request, segments):
+        return ClosedResource()
+
+
+class OpenResource(resource.Resource):
+
+    @resource.GET(accept='html')
+    @templating.page('open.html')
+    def html(self, request):
+        return {}
+
+    @resource.child()
+    def another(self, request, segments):
+        return AnotherResource('Open')
+
+
+class ClosedResource(resource.Resource):
+
+    @resource.GET(accept='html')
+    @templating.page('closed.html')
+    def html(self, request):
+        return {}
+
+    @resource.child()
+    def another(self, request, segments):
+        return AnotherResource('Closed')
+
+
+class AnotherResource(resource.Resource):
+
+    def __init__(self, child_of):
+        self.child_of = child_of
+
+    @resource.GET(accept='html')
+    @templating.page('another.html')
+    def html(self, request):
+        return {'child_of': self.child_of}
 
