49d317a973a01dd87cbbab7c167bfbf39bbe587a
Matt Goodall <matt.goodall@gmail.com>
2015-02-06 10:53:41 +0000

    Improve setup.py format; use README for long description.

--- setup.py
+++ setup.py
@@ -1,52 +1,58 @@
 from setuptools import setup, find_packages
-import sys, os
+import codecs
 
-version = '0.13.2'
+# Get the long description from the relevant file
+with codecs.open('README', encoding='utf-8') as f:
+    long_description = f.read()
 
-setup(name='restish',
-      version=version,
-      description="WSGI framework/library for building resource- and rest- oriented web sites",
-      long_description="""\
-Restish is a simple to use, lightweight WSGI web framework and library with a
-strong focus on resources, request/response, URLs and content negotiation.
-Restish has very few dependencies and does not assume any particular templating
-or database engine.
+setup(
+    name='restish',
 
-      Changlog at `http://github.com/ish/restish/raw/master/CHANGELOG <http://github.com/ish/restish/raw/master/CHANGELOG>`_
+    version='0.13.2',
 
-""",
-      classifiers=[
-          "Development Status :: 4 - Beta",
-          "Environment :: Web Environment",
-          "Framework :: Paste",
-          "Intended Audience :: Developers",
-          "License :: OSI Approved :: BSD License",
-          "Operating System :: OS Independent",
-          "Programming Language :: Python :: 2",
-          "Topic :: Internet :: WWW/HTTP :: Dynamic Content",
-          "Topic :: Internet :: WWW/HTTP :: WSGI",
-          "Topic :: Software Development :: Libraries :: Python Modules",
-      ], # Get strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
-      keywords='web wsgi rest framework',
-      author='ish',
-      author_email='developers@ish.io',
-      url='http://ish.io/projects/show/restish',
-      license='BSD',
-      packages=find_packages(exclude=['ez_setup', 'examples', 'tests']),
-      package_data={
-          'restish': ['restish/pastertemplate/*'],
-      },
-      zip_safe=False,
-      install_requires=[
-          # -*- Extra requirements: -*-
-          'mimeparse>=0.1.3',
-          'WebOb',
-      ],
-      entry_points="""
-      # -*- Entry points: -*-
-      [paste.paster_create_template]
-      restish = restish.pastertemplate:RestishTemplate
-      """,
-      test_suite="restish.tests",
-      tests_require=['WebTest', 'Jinja2', 'mako', 'Genshi', 'Tempita', 'Django'],
-      )
+    description="WSGI framework/library for building resource- and rest- oriented web sites",
+    long_description=long_description,
+
+    url='http://ish.io/projects/show/restish',
+
+    author='ish',
+    author_email='developers@ish.io',
+
+    license='BSD',
+
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Environment :: Web Environment",
+        "Framework :: Paste",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: BSD License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 2",
+        "Topic :: Internet :: WWW/HTTP :: Dynamic Content",
+        "Topic :: Internet :: WWW/HTTP :: WSGI",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+    ], # Get strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
+
+    keywords='web wsgi rest framework',
+
+    packages=find_packages(exclude=['ez_setup', 'examples', 'tests']),
+    package_data={
+        'restish': ['restish/pastertemplate/*'],
+    },
+    zip_safe=False,
+
+    install_requires=[
+        # -*- Extra requirements: -*-
+        'mimeparse>=0.1.3',
+        'WebOb',
+    ],
+
+    test_suite="restish.tests",
+    tests_require=['WebTest', 'Jinja2', 'mako', 'Genshi', 'Tempita', 'Django'],
+
+    entry_points="""
+    # -*- Entry points: -*-
+    [paste.paster_create_template]
+    restish = restish.pastertemplate:RestishTemplate
+    """,
+)
